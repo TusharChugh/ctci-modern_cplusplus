edc624be7becb9d826820db5aaebc85f09b5c74b
Tushar Chugh <techie2sh@gmail.com>
2018-04-14 21:13:07 -0400

    add readme to arrays_strings

---
+++ include/arrays_strings/README.md
@@ -0,0 +1,12 @@
+# Common questions and Test cases
+1. Can the input be empty?
+2. Upper case vs lower case
+3. How big the input can be?
+4. Can we use extra space?
+
+# Common patterns
+1. Create an array/vector of size (= NUM ASCII)
+2. Create an array/vector of size (= NUM INPUTS)
+3. Hash sets and hash maps are useful. 
+4. In STL unordered_map and unordered_set containers provide O(1) are implemented using hash
+5. In STL map and set are ordered and are implemeneted by Red-Black trees (O(log(N)) look-up time)
---
+++ include/arrays_strings/palindrome_permutation.h
@@ -0,0 +1,16 @@
+/**
+ * @brief Palindrome Permutation
+ *  Check if a string is a permutation of a palin-drome.
+ *  A palindrome is a word or phrase that is the same forwards and backwards.
+ *  A permutation is a rearrangement of letters. The palindrome does not need to be limited to just
+ *  dictionary words.
+ *
+ *  EXAMPLE Input: Tact Coa Output: True (permutations: "taco cat". "atco cta". etc.)
+ *
+ * @file palindrome_permutation.h
+ * @author Tushar Chugh
+ */
+
+#include <string>
+
+bool is_palindrom_permutation( const std::string& input ) {}
