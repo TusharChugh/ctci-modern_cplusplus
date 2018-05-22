bfced8203eb3bf62b9eaebde18b15e80bd24d449
TusharChugh <techie2sh@gmail.com>
2018-05-21 20:41:24 -0400

    add successor to tree graphs

---
+++ include/trees_graphs/successor.h
@@ -0,0 +1,9 @@
+/**
+ * @brief Successor Write an algorithm to find the "next" node (i.e., in-order successor) of a given
+ * node in a binary search tree. You may assume that each node has a link to its parent.
+ *
+ * @file successor.h
+ * @author Tushar Chugh
+ */
+
+// Already implemented in the bst iterator implementation
