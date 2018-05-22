ae8bbf75a94b601182782bf299d84e681d1ee875
Tushar Chugh <techie2sh@gmail.com>
2018-05-22 19:06:34 -0400

    Add build order trees graph

--- include/trees_graphs/build_order.h
+++ include/trees_graphs/build_order.h
@@ -18,21 +18,34 @@
 #include <vector>
 
 namespace algorithm {
+// better would be to implement a class the dfs function has too many arguments - ugly
+
+void dfs_build(char project, std::vector<char>& ordered_projects, const std::unordered_multimap<char, char>& dependency_pairs,
+               std::unordered_set<char>& built, std::unordered_set<char>& built_same_node) {
+    built.insert(project);
+    built_same_node.insert(project);
+
+    auto range_adj = dependency_pairs.equal_range(project);
+    for (auto start = range_adj.first; start != range_adj.second; ++start) {
+        auto proj = (*start).second;
+        if(built_same_node.find(proj) != built_same_node.end())
+            std::__throw_invalid_argument("error: cyclic dependency");
+        if(built.find(proj) == built.end())
+            dfs_build(proj, ordered_projects, dependency_pairs, built, built_same_node);
+    }
+    ordered_projects.push_back(project);
+}
 
 std::vector<char> build_order( std::vector<char> projects,
-                               std::unordered_multimap<char, char> dependency_pairs ) {
+                               const std::unordered_multimap<char, char>& dependency_pairs ) {
     std::vector<char> ordered_projects;
     std::unordered_set<char> built;
 
-    // for ( const auto& project : projects ) {
-    //     std::stack<char>
-    //     if ( built.find( project ) == built.end() ) {
-    //         built.insert( project );
-    //         auto range_adj = dependency_pairs.equal_range( project );
-    //         for ( auto start = range_adj.first; start != range_adj.second; ++start ) {
-
-    //         }
-    //     }
-    // }
+     for ( const auto& project : projects ) {
+         std::unordered_set<char> built_same_root;
+         if(built.find(project) == built.end())
+            dfs_build(project, ordered_projects, dependency_pairs, built, built_same_root);
+     }
+    return  ordered_projects;
 }
 } // namespace algorithm
---
+++ test/tests/trees_graphs/build_order_test.cpp
@@ -0,0 +1,25 @@
+#include "include/trees_graphs/build_order.h"
+#include <gtest/gtest.h>
+
+TEST(BUILD_ORDER, DEMO) {
+    std::vector<char> projects = {'a', 'b', 'c', 'd', 'e', 'f'};
+    std::unordered_multimap<char, char> dependency_pairs = {{'d', 'a'}, {'b', 'f'}, {'d', 'b'}, {'a', 'f'}, {'c', 'd'}};
+    std::vector<char> expected_output = {'f', 'a', 'b', 'd', 'c', 'e'};
+
+    ASSERT_EQ(expected_output, algorithm::build_order(projects, dependency_pairs));
+}
+
+TEST(BUILD_ORDER, CYCLIC_TEST) {
+    std::vector<char> projects = {'a', 'b', 'c', 'd', 'e', 'f'};
+    std::unordered_multimap<char, char> dependency_pairs = {{'d', 'a'}, {'b', 'f'}, {'d', 'b'}, {'a', 'f'}, {'c', 'd'}, {'a', 'd'}};
+
+    ASSERT_ANY_THROW(algorithm::build_order(projects, dependency_pairs));
+}
+
+TEST(BUILD_ORDER, EMPTY_TEST) {
+    std::vector<char> projects;
+    std::unordered_multimap<char, char> dependency_pairs;
+    std::vector<char> expected_output;
+
+    ASSERT_EQ(expected_output, algorithm::build_order(projects, dependency_pairs));
+}
