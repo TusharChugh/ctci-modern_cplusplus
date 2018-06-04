4b97717b6d1958a3d5db0a5e2c251962ab4ef5ad
TusharChugh <techie2sh@gmail.com>
2018-06-04 09:47:26 -0400

    add perms without dups

---
+++ include/recursion_dp/permutation_without_dups.h
@@ -0,0 +1,35 @@
+/**
+ * @brief Permutations without Dups
+ * Write a method to compute all permutations of a string of unique characters.
+ *
+ * @file permutation_without_dups.h
+ * @author Tushar Chugh
+ */
+
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace algorithm {
+std::string append_at_pos( const std::string& input, char letter, size_t pos ) {
+    return input.substr( 0, pos ) + letter + input.substr( pos, input.length() - pos );
+}
+
+std::vector<std::string> perms_no_dups( std::string input ) {
+    std::vector<std::string> result;
+    if ( input.empty() ) {
+        result.push_back( "" );
+        return result;
+    }
+    auto front_letter = input.front();
+    auto words        = perms_no_dups(
+        input.substr( 1, input.length() - 1 ) ); // duplicates of input - first letter
+    for ( auto word : words ) {
+        for ( size_t pos = 0; pos <= word.length(); ++pos ) {
+            result.push_back( append_at_pos( word, front_letter, pos ) );
+        }
+    }
+    return result;
+}
+} // namespace algorithm
