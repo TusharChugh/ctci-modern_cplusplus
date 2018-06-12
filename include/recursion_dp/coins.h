74b420990980cef71ed0a5a16755c5de4c2cb021
TusharChugh <techie2sh@gmail.com>
2018-06-11 21:54:57 -0400

    add coins dp

---
+++ include/recursion_dp/coins.h
@@ -0,0 +1,45 @@
+/**
+ * @brief Coins
+ * Given an infinite number of quarters (25 cents), dimes (10 cents), nickels (5 cents),  and
+ * pennies (1 cent), write code to calculate the number of ways of representing n cents.
+ *
+ * @file coins.h
+ * @author Tushar Chugh
+ */
+
+#pragma once
+
+#include <iostream>
+#include <vector>
+
+namespace algorithm {
+int make_change( const std::vector<int>& coins, int sum, int index ) {
+    if ( sum == 0 ) return 1;
+    if ( sum < 0 ) return 0;
+    if ( index <= 0 ) return 0;
+
+    return make_change( coins, sum, index - 1 ) +
+           make_change( coins, sum - coins[index - 1], index );
+}
+
+int make_change_recursive( const std::vector<int>& coins, int sum ) {
+    return make_change( coins, sum, coins.size() );
+}
+
+int make_change_dp( const std::vector<int>& coins, unsigned sum ) {
+    std::vector<std::vector<int>> table( coins.size() + 1, std::vector<int>( sum + 1 ) );
+    table[0][0] = 1;
+    for ( auto row = 1; row <= coins.size(); ++row ) {
+        for ( auto col = 0; col <= sum; ++col ) {
+            auto sum_excluding = ( row - 1 >= 0 ) ? table[row - 1][col] : 0;
+            auto sum_including =
+                ( col - coins[row - 1] >= 0 ) ? table[row][col - coins[row - 1]] : 0;
+            table[row][col] = sum_excluding + sum_including;
+
+            std::cout << table[row][col] << " ";
+        }
+        std::cout << std::endl;
+    }
+    return table[coins.size()][sum];
+}
+} // namespace algorithm
---
+++ test/tests/recursion_dp/coins_test.cpp
@@ -0,0 +1,22 @@
+#include "include/recursion_dp/coins.h"
+#include <gtest/gtest.h>
+
+TEST( COINS, THREE_COINS_RECURSIVE ) {
+    std::vector<int> coins = {1, 2, 3};
+    ASSERT_EQ( 4, algorithm::make_change_recursive( coins, 4 ) );
+}
+
+TEST( COINS, FIVE_COINS_RECURSIVE ) {
+    std::vector<int> coins = {1, 2, 3, 4, 5};
+    ASSERT_EQ( 7, algorithm::make_change_dp( coins, 5 ) );
+}
+
+TEST( COINS, THREE_COINS_DP ) {
+    std::vector<int> coins = {1, 2, 3};
+    ASSERT_EQ( 4, algorithm::make_change_dp( coins, 4 ) );
+}
+
+TEST( COINS, FIVE_COINS_DP ) {
+    std::vector<int> coins = {1, 2, 3, 4, 5};
+    ASSERT_EQ( 7, algorithm::make_change_dp( coins, 5 ) );
+}
