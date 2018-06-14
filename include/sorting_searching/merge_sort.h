049415920a3bbedd9d0e5a22c7e1466fcd64df11
TusharChugh <techie2sh@gmail.com>
2018-06-13 21:03:40 -0400

    add merge sort

---
+++ include/sorting_searching/merge_sort.h
@@ -0,0 +1,60 @@
+/**
+ * @brief Templated implementation of merge sort
+ *
+ * @file merge_sort.h
+ * @author Tushar Chugh
+ */
+
+#pragma once
+
+#include <algorithm>
+#include <cstddef>
+
+namespace algorithm {
+
+template <typename RandomIt, typename Compare>
+void merge( RandomIt first, RandomIt mid, RandomIt last, Compare comp,
+            typename std::vector<typename std::iterator_traits<RandomIt>::value_type>::iterator
+                copy_iterator ) {
+
+    auto first_t         = first;
+    auto copy_iterator_t = copy_iterator;
+    auto mid_t           = mid;
+
+    while ( first_t < mid && mid_t < last )
+        *( copy_iterator_t++ ) = comp( *first_t, *mid_t ) ? *( first_t++ ) : *( mid_t++ );
+
+    copy_iterator_t = std::move( first_t, mid, copy_iterator_t );
+    copy_iterator_t = std::move( mid_t, last, copy_iterator_t );
+
+    std::move( copy_iterator, copy_iterator_t, first );
+}
+
+template <typename RandomIt, typename Compare>
+void merge_sort( RandomIt first, RandomIt last, Compare comp,
+                 typename std::vector<typename std::iterator_traits<RandomIt>::value_type>::iterator
+                     copy_iterator ) {
+    const size_t size = std::distance( first, last );
+    if ( size <= 1 ) return;
+
+    auto mid = first;
+    std::advance( mid, ( size / 2 ) );
+
+    merge_sort( first, mid, comp, copy_iterator );
+    merge_sort( mid, last, comp, copy_iterator );
+
+    merge( first, mid, last, comp, copy_iterator );
+}
+
+template <typename RandomIt, typename Compare>
+void merge_sort( RandomIt first, RandomIt last, Compare comp ) {
+    std::vector<typename std::iterator_traits<RandomIt>::value_type> input_copy(
+        std::distance( first, last ) );
+    merge_sort( first, last, comp, input_copy.begin() );
+}
+
+template <class RandomIt> void merge_sort( RandomIt first, RandomIt last ) {
+    merge_sort( first, last, std::less<typename std::iterator_traits<RandomIt>::value_type>() );
+}
+
+} // namespace algorithm
---
+++ test/tests/sorting_searching/merge_sort_test.cpp
@@ -0,0 +1,122 @@
+#include "include/sorting_searching/merge_sort.h"
+#include <gtest/gtest.h>
+#include <stdlib.h>
+#include <algorithm>
+#include <vector>
+
+TEST( MERGESORT, MERGESORT_TWO_UNSORTED_ELEMENTS_TRUE_Test ) {
+    std::vector<int> input1{5, 2};
+    std::vector<int> input2{5, 2};
+    std::sort( input1.begin(), input1.end() );
+    algorithm::merge_sort( input2.begin(), input2.end() );
+    ASSERT_TRUE( std::equal( input1.begin(), input1.end(), input2.begin() ) );
+}
+
+TEST( MERGESORT, MERGESORT_TWO_SORTED_ELEMENTS_TRUE_Test ) {
+    std::vector<int> input1{2, 5};
+    std::vector<int> input2{2, 5};
+    std::sort( input1.begin(), input1.end() );
+    algorithm::merge_sort( input2.begin(), input2.end() );
+    ASSERT_TRUE( std::equal( input1.begin(), input1.end(), input2.begin() ) );
+}
+
+TEST( MERGESORT, MERGESORT_VECTOR__ODD_ELEMENTS_TRUE_Test ) {
+    std::vector<int> input1{2, 5, 1, 4, 3, 7, 5};
+    std::vector<int> input2{2, 5, 1, 4, 3, 7, 5};
+    std::sort( input1.begin(), input1.end() );
+    algorithm::merge_sort( input2.begin(), input2.end() );
+    ASSERT_TRUE( std::equal( input1.begin(), input1.end(), input2.begin() ) );
+}
+
+TEST( MERGESORT, MERGESORT_VECTOR_EVEN_ELEMENTS_TRUE_Test ) {
+    std::vector<int> input1{2, 5, 1, 4, 3, 7};
+    std::vector<int> input2{2, 5, 1, 4, 3, 7};
+    std::sort( input1.begin(), input1.end() );
+    algorithm::merge_sort( input2.begin(), input2.end() );
+    ASSERT_TRUE( std::equal( input1.begin(), input1.end(), input2.begin() ) );
+}
+
+TEST( MERGESORT, MERGESORT_VECTOR_PRESORTED_ELEMENTS_TRUE_Test ) {
+    std::vector<int> input1{1, 2, 3, 4, 5};
+    std::vector<int> input2{1, 2, 3, 4, 5};
+    std::sort( input1.begin(), input1.end() );
+    algorithm::merge_sort( input2.begin(), input2.end() );
+    ASSERT_TRUE( std::equal( input1.begin(), input1.end(), input2.begin() ) );
+}
+
+TEST( MERGESORT, MERGESORT_VECTOR_ELEMENTS_FALSE_Test ) {
+    std::vector<int> input1{2, 5, 1, 4, 3, 7, 5};
+    std::vector<int> input2{2, 5, 8, 4, 3, 7, 5};
+    std::sort( input1.begin(), input1.end() );
+    algorithm::merge_sort( input2.begin(), input2.end() );
+    ASSERT_FALSE( std::equal( input1.begin(), input1.end(), input2.begin() ) );
+}
+
+TEST( MERGESORT, MERGESORT_ARRAY_ELEMENTS_Test ) {
+    std::array<int, 7> input1{2, 5, 1, 4, 3, 7, 5};
+    std::array<int, 7> input2{2, 5, 1, 4, 3, 7, 5};
+    std::sort( input1.begin(), input1.end() );
+    algorithm::merge_sort( input2.begin(), input2.end() );
+    ASSERT_TRUE( std::equal( input1.begin(), input1.end(), input2.begin() ) );
+}
+
+TEST( MERGESORT, MERGESORT_STRING_ELEMENTS_Test ) {
+    std::string input1( "Hello World" );
+    std::string input2( "Hello World" );
+    std::sort( input1.begin(), input1.end() );
+    algorithm::merge_sort( input2.begin(), input2.end() );
+    ASSERT_TRUE( std::equal( input1.begin(), input1.end(), input2.begin() ) );
+}
+
+TEST( MERGESORT, MERGESORT_VECTOR_FLOAT_ELEMENTS_TRUE_Test ) {
+    std::vector<float> input1{2.5, 2.4, 2.3, 2.10, 3.4, 7.6, 1.2};
+    std::vector<float> input2{2.5, 2.4, 2.3, 2.10, 3.4, 7.6, 1.2};
+    std::sort( input1.begin(), input1.end() );
+    algorithm::merge_sort( input2.begin(), input2.end() );
+    ASSERT_TRUE( std::equal( input1.begin(), input1.end(), input2.begin() ) );
+}
+
+TEST( MERGESORT, MERGESORT_VECTOR_CHAR_ELEMENTS_TRUE_Test ) {
+    std::vector<char> input1{'a', 'd', 'b', 'c'};
+    std::vector<char> input2{'a', 'd', 'b', 'c'};
+    std::sort( input1.begin(), input1.end() );
+    algorithm::merge_sort( input2.begin(), input2.end() );
+    ASSERT_TRUE( std::equal( input1.begin(), input1.end(), input2.begin() ) );
+}
+
+TEST( MERGESORT, MERGESORT_EMPTY_VECTOR_ELEMENTS_TRUE_Test ) {
+    std::vector<int> input1;
+    std::vector<int> input2;
+    std::sort( input1.begin(), input1.end() );
+    algorithm::merge_sort( input2.begin(), input2.end() );
+    ASSERT_TRUE( std::equal( input1.begin(), input1.end(), input2.begin() ) );
+}
+
+TEST( MERGESORT, MERGESORT_VECTOR_REVERSE_ELEMENTS_FALSE_Test ) {
+    const size_t size = 10000;
+    std::vector<unsigned int> input1( size );
+    std::vector<unsigned int> input2( size );
+
+    for ( unsigned int index = size; index > 0; --index ) {
+        input1.push_back( index );
+        input2.push_back( index );
+    }
+    std::sort( input1.begin(), input1.end() );
+    algorithm::merge_sort( input2.begin(), input2.end() );
+    ASSERT_TRUE( std::equal( input1.begin(), input1.end(), input2.begin() ) );
+}
+
+TEST( MERGESORT, MERGESORT_VECTOR_RANDOM_ELEMENTS_FALSE_Test ) {
+    const size_t size = 10000;
+    std::vector<unsigned int> input1( size );
+    std::vector<unsigned int> input2( size );
+
+    for ( unsigned int index = 0; index < size; ++index ) {
+        unsigned int num = rand();
+        input1.push_back( num );
+        input2.push_back( num );
+    }
+    std::sort( input1.begin(), input1.end() );
+    algorithm::merge_sort( input2.begin(), input2.end() );
+    ASSERT_TRUE( std::equal( input1.begin(), input1.end(), input2.begin() ) );
+}
