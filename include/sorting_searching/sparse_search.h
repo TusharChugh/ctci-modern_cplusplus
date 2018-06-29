10a88b0e742afba697a11accf71d077894aad3fd
TusharChugh <techie2sh@gmail.com>
2018-06-29 17:16:51 -0400

    add sorting searching

--- include/sorting_searching/sparse_search.h
+++ include/sorting_searching/sparse_search.h
@@ -12,4 +12,44 @@
 
 #pragma once
 
-namespace algorithm {}
+#include <string>
+#include <vector>
+
+namespace algorithm {
+int get_nearest_nonempty_leftindex( const std::vector<std::string>& input, int index, int first,
+                                    int last ) {
+    if ( !input.at( index ).empty() ) return index;
+    int left  = index - 1;
+    int right = index + 1;
+
+    while ( true ) {
+        if ( left < first && right > last ) return -1;
+        if ( right <= last && !input[right].empty() ) return right;
+        if ( left >= first && !input[left].empty() ) return left;
+
+        ++right;
+        --left;
+    }
+}
+
+int binary_search( const std::vector<std::string>& input, const std::string& query, int first,
+                   int last ) {
+    if ( first > last ) return -1;
+    int mid = ( first + last ) / 2;
+
+    mid = get_nearest_nonempty_leftindex( input, mid, first, last );
+
+    if ( mid < 0 ) return mid;
+
+    if ( query == input.at( mid ) ) return mid;
+    if ( query < input.at( mid ) )
+        binary_search( input, query, first, mid - 1 );
+    else
+        binary_search( input, query, mid + 1, last );
+}
+
+int sparse_search( const std::vector<std::string>& input, const std::string& query ) {
+    if ( input.empty() ) return -1;
+    return binary_search( input, query, 0, input.size() - 1 );
+}
+} // namespace algorithm
--- test/tests/sorting_searching/sparse_search_test.cpp
+++ test/tests/sorting_searching/sparse_search_test.cpp
@@ -1,2 +1,8 @@
 #include "include/sorting_searching/sparse_search.h"
 #include <gtest/gtest.h>
+
+TEST( SPARSE_SEARCH, DEMO ) {
+    std::vector<std::string> input = {"at",  "", "", "",    "ball", "", "",
+                                      "car", "", "", "dad", "",     ""};
+    ASSERT_EQ( 4, algorithm::sparse_search( input, "ball" ) );
+}
