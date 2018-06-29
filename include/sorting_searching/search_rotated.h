5304badc5e585515a408944e433a415f8d5cc94c
TusharChugh <techie2sh@gmail.com>
2018-06-28 20:31:48 -0400

    add comments to the header

--- include/sorting_searching/search_rotated.h
+++ include/sorting_searching/search_rotated.h
@@ -1,11 +1,12 @@
 /**
- * Search in Rotated Array: Given a sorted array of n integers that has been rotated an unknown
-number of times, write code to find an element in the array. You may assume that the array was
-originally sorted in increasing order.
-EXAMPLE
-Input Find 5 in array {15, 16, 19, 20, 25, 1, 3, 4, 5, 7, 10, 14}
-Output 8 (the index of 5 in the array)
-**/
+ * @brief  Search in Rotated Array: Given a sorted array of n integers that has been rotated an
+ * unknown number of times, write code to find an element in the array. You may assume that the
+ * array was originally sorted in increasing order. EXAMPLE Input Find 5 in array {15, 16, 19, 20,
+ * 25, 1, 3, 4, 5, 7, 10, 14} Output 8 (the index of 5 in the array)
+ *
+ * @file search_rotated.h
+ * @author Tushar Chugh
+ */
 
 #pragma once
 
@@ -28,38 +29,35 @@ Output 8 (the index of 5 in the array)
  */
 
 namespace algorithm {
-    int search_rotated(const std::vector<int>& input, int elem, int first, int last) {
-        int mid = (first + last)/2;
+int search_rotated( const std::vector<int>& input, int elem, int first, int last ) {
+    int mid = ( first + last ) / 2;
 
-        if(input[mid] == elem) return mid;
+    if ( input[mid] == elem ) return mid;
 
-        if(last < first) return -1;
+    if ( last < first ) return -1;
 
-        if(input[mid] > input[first]) {
-            if(input[first] <= elem && elem < input[mid])
-                return search_rotated(input, elem, first, mid - 1);
-            else
-                return search_rotated(input, elem, mid + 1, last);
-        }
-        else if (input[mid] < input[first]) {
-            if(input[mid] < elem && elem <= input[last])
-                return search_rotated(input, elem, mid + 1, last);
-            else
-                return search_rotated(input, elem, first, mid - 1);
-        }
+    if ( input[mid] > input[first] ) {
+        if ( input[first] <= elem && elem < input[mid] )
+            return search_rotated( input, elem, first, mid - 1 );
+        else
+            return search_rotated( input, elem, mid + 1, last );
+    } else if ( input[mid] < input[first] ) {
+        if ( input[mid] < elem && elem <= input[last] )
+            return search_rotated( input, elem, mid + 1, last );
+        else
+            return search_rotated( input, elem, first, mid - 1 );
+    } else {
+        if ( input[mid] != input[last] )
+            return search_rotated( input, elem, mid + 1, last );
         else {
-            if(input[mid] != input[last])
-                return search_rotated(input, elem, mid + 1, last);
-            else {
-                int result = search_rotated(input, elem, first, mid - 1);
-                if(result == -1)
-                    return search_rotated(input, elem, mid + 1, last);
-                return result;
-            }
+            int result = search_rotated( input, elem, first, mid - 1 );
+            if ( result == -1 ) return search_rotated( input, elem, mid + 1, last );
+            return result;
         }
-    }
-
-    int search_rotated(const std::vector<int>& input, int elem) {
-        return search_rotated(input, elem, 0, input.size() - 1);
     }
 }
+
+int search_rotated( const std::vector<int>& input, int elem ) {
+    return search_rotated( input, elem, 0, input.size() - 1 );
+}
+} // namespace algorithm
--- include/sorting_searching/sorted_merge.h
+++ include/sorting_searching/sorted_merge.h
@@ -1,24 +1,31 @@
+/**
+ * @brief Sorted Merge: You are given two sorted arrays, A and B, where A has a large enough buffer
+ * at the end to hold B. Write a method to merge B into A in sorted order.
+ *
+ * @file sorted_merge.h
+ * @author Tushar Chugh
+ */
+
 #pragma once
 
 #include <vector>
 
 namespace algorithm {
-    void sorted_merge(std::vector<int>& A, const std::vector<int>& B, int last_a, int last_b) {
-        int merged_index = A.size() - 1;
-        while(last_b >= 0) {
-            if(last_a >=0 && A[last_a] > B[last_b]) {
-                A[merged_index] = A[last_a];
-                --last_a;
-            }
-            else {
-                A[merged_index] = B[last_b];
-                --last_b;
-            }
-            --merged_index;
+void sorted_merge( std::vector<int>& A, const std::vector<int>& B, int last_a, int last_b ) {
+    int merged_index = A.size() - 1;
+    while ( last_b >= 0 ) {
+        if ( last_a >= 0 && A[last_a] > B[last_b] ) {
+            A[merged_index] = A[last_a];
+            --last_a;
+        } else {
+            A[merged_index] = B[last_b];
+            --last_b;
         }
-    }
-
-    void sorted_merge(std::vector<int>& A, const std::vector<int>& B, int num_elements_a) {
-        sorted_merge(A, B, num_elements_a - 1, B.size() - 1);
+        --merged_index;
     }
 }
+
+void sorted_merge( std::vector<int>& A, const std::vector<int>& B, int num_elements_a ) {
+    sorted_merge( A, B, num_elements_a - 1, B.size() - 1 );
+}
+} // namespace algorithm
