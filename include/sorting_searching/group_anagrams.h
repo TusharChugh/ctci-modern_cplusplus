c1e19ea97fcb022cde9169a0d8487c41ddb3daaf
TusharChugh <techie2sh@gmail.com>
2018-06-29 15:52:34 -0400

    add group anagrams

--- include/sorting_searching/group_anagrams.h
+++ include/sorting_searching/group_anagrams.h
@@ -8,4 +8,25 @@
 
 #pragma once
 
-namespace algorithm {}
+#include <algorithm>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+namespace algorithm {
+void group_anagrams( std::vector<std::string>& input ) {
+    std::unordered_multimap<std::string, std::string> sorted_input_map;
+
+    for ( const auto& word : input ) {
+        std::string sorted_word = word;
+        std::sort( sorted_word.begin(), sorted_word.end() );
+        sorted_input_map.emplace( sorted_word, word );
+    }
+
+    size_t index = 0;
+
+    for ( const auto& bucket : sorted_input_map ) {
+        input.at( index++ ) = bucket.second;
+    }
+}
+} // namespace algorithm
--- test/tests/sorting_searching/group_anagrams_test.cpp
+++ test/tests/sorting_searching/group_anagrams_test.cpp
@@ -1,2 +1,20 @@
 #include "include/sorting_searching/group_anagrams.h"
 #include <gtest/gtest.h>
+
+// NOTE: The tests are hardcode and are ordered depedent. It shouldn't be the case
+
+TEST( GROUP_ANAGRAMS, TWO_ANAGRAMS ) {
+    std::vector<std::string> input = {"hello", "hell", "elloh"};
+    algorithm::group_anagrams( input );
+    std::vector<std::string> expected_output = {"hell", "elloh", "hello"};
+    ASSERT_EQ( expected_output, input );
+}
+
+TEST( GROUP_ANAGRAMS, TWO_PAIRS_ANAGRAMS_SEVEN_WORDS ) {
+    std::vector<std::string> input = {"abccdde", "abc", "abccd",  "addccbe",
+                                      "accdb",   "abd", "aebcdcd"};
+    algorithm::group_anagrams( input );
+    std::vector<std::string> expected_output = {"abd",     "abc",   "aebcdcd", "addccbe",
+                                                "abccdde", "accdb", "abccd"};
+    ASSERT_EQ( expected_output, input );
+}
