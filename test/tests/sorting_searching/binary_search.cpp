68439558e4a3a777ac9062dc8ba28f84735cc0f0
Tushar Chugh <techie2sh@gmail.com>
2018-06-29 23:55:26 -0400

    fix group anagram test

---
+++ test/tests/sorting_searching/binary_search.cpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <include/gtest/gtest.h>
+#include "include/sorting_searching/binary_search.h"
+
+TEST(BINARY_SEARCH, FIVE_ELEMENTS) {
+    std::vector<int> input = {1, 2, 4, 8, 10};
+    ASSERT_EQ(0, algorithm::binary_search(input, 1));
+    ASSERT_EQ(1, algorithm::binary_search(input, 2));
+    ASSERT_EQ(2, algorithm::binary_search(input, 4));
+    ASSERT_EQ(3, algorithm::binary_search(input, 8));
+    ASSERT_EQ(4, algorithm::binary_search(input, 10));
+}
--- test/tests/sorting_searching/group_anagrams_test.cpp
+++ test/tests/sorting_searching/group_anagrams_test.cpp
@@ -1,20 +1,23 @@
 #include "include/sorting_searching/group_anagrams.h"
 #include <gtest/gtest.h>
 
-// NOTE: The tests are hardcode and are ordered depedent. It shouldn't be the case
-
 TEST( GROUP_ANAGRAMS, TWO_ANAGRAMS ) {
     std::vector<std::string> input = {"hello", "hell", "elloh"};
     algorithm::group_anagrams( input );
-    std::vector<std::string> expected_output = {"hell", "elloh", "hello"};
-    ASSERT_EQ( expected_output, input );
+    ASSERT_EQ( 1, std::abs(std::find(input.begin(), input.end(), "hello") - std::find(input.begin(), input.end(), "elloh")));
 }
 
 TEST( GROUP_ANAGRAMS, TWO_PAIRS_ANAGRAMS_SEVEN_WORDS ) {
     std::vector<std::string> input = {"abccdde", "abc", "abccd",  "addccbe",
                                       "accdb",   "abd", "aebcdcd"};
     algorithm::group_anagrams( input );
-    std::vector<std::string> expected_output = {"abd",     "abc",   "aebcdcd", "addccbe",
-                                                "abccdde", "accdb", "abccd"};
-    ASSERT_EQ( expected_output, input );
+
+    auto index1 = std::find(input.begin(), input.end(), "aebcdcd");
+    auto index2 = std::find(input.begin(), input.end(), "addccbe");
+    auto index3 = std::find(input.begin(), input.end(), "abccdde");
+
+
+    ASSERT_EQ( 2, std::max({index1, index2, index3}) - std::min({index1, index2, index3}));
+
+    ASSERT_EQ( 1, std::abs(std::find(input.begin(), input.end(), "accdb") - std::find(input.begin(), input.end(), "abccd")));
 }
