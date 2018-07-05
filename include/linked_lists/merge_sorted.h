7a0c808145bd7da95ae954101a2a8e83c8a87f2c
Tushar Chugh <techie2sh@gmail.com>
2018-07-05 18:23:10 -0400

    add merge two sorted lists

---
+++ include/linked_lists/merge_sorted.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "include/linked_lists/simple_int_list.h"
+
+namespace algorithm {
+    List_Node* merge_sorted(List_Node* head1, List_Node* head2) {
+        List_Node* result = new List_Node();
+        List_Node* result_head = result;
+
+        while(head1 && head2) {
+            if(head1->val <= head2->val){
+                result->next = head1;
+                head1 = head1->next;
+            }
+            else {
+                result->next = head2;
+                head2 = head2->next;
+            }
+            result = result->next;
+        }
+
+        if(head1) {
+            result ->next = head1;
+        }
+        if(head2) {
+            result ->next = head2;
+        }
+        
+        return result_head->next;
+    }
+}
---
+++ test/tests/linked_lists/merge_sorted_test.cpp
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+#include "include/linked_lists/merge_sorted.h"
+
+TEST(MERGE_SORTED, DEMO){
+    auto list1 = make_list({5, 4, 1});
+    auto list2 = make_list({6, 3, 2});
+    auto merged = algorithm::merge_sorted(list1, list2);
+    int expected_value = 1;
+//    while(merged) {
+//        ASSERT_EQ(expected_value++, merged->val);
+//        merged = merged->next;
+//    }
+}
